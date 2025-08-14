e5d08e9a1de3ab724d97a2f2742ec90859e08e8b
Laurie Crean <133490867+lmcrean@users.noreply.github.com>
2025-08-14 12:17:17 +0000

    Add support for generated columns in column metadata query

--- internal/queries/queries.sql
+++ internal/queries/queries.sql
@@ -92,8 +92,18 @@ SELECT
     COALESCE(coll.collname, '')::TEXT AS collation_name,
     COALESCE(collation_namespace.nspname, '')::TEXT AS collation_schema_name,
     COALESCE(
-        pg_catalog.pg_get_expr(d.adbin, d.adrelid), ''
+        CASE 
+            WHEN a.attgenerated = 's' THEN ''
+            ELSE pg_catalog.pg_get_expr(d.adbin, d.adrelid)
+        END, ''
     )::TEXT AS default_value,
+    COALESCE(
+        CASE
+            WHEN a.attgenerated = 's' THEN pg_catalog.pg_get_expr(d.adbin, d.adrelid)
+            ELSE ''
+        END, ''
+    )::TEXT AS generation_expression,
+    (a.attgenerated = 's') AS is_generated,
     a.attnotnull AS is_not_null,
     a.attlen AS column_size,
     a.attidentity::TEXT AS identity_type,
